46f64852fa65db9e840f034938bebf30d86a1d0a
Tucker Tirey <tucker.r.tirey@gmail.com>
2024-03-20 08:45:03 -0400

    Added buttons to clear and save tasks - these still need to be implemented.

--- todo.py
+++ todo.py
@@ -4,9 +4,10 @@ import task
 
 ##
 ## To Do
-##  - Finish button functionality to add a new task
+##  X Finish button functionality to add a new task
 ##  - Implement a button to save unchecked tasks to a file
 ##  - Implement loading in tasks from file (or db?)
+##  - Implement button to clear tasks
 
 # Class definition for the frame that holds the checkboxes
 class CheckboxFrame(customtkinter.CTkFrame):
@@ -56,23 +57,35 @@ class App(customtkinter.CTk):
         #self.grid_rowconfigure(1, weight=1)
 
         # Create the " Add Task" button; requires additional work
-        self.button = customtkinter.CTkButton(self, text="Add Task", command=self.button_event)
+        self.button = customtkinter.CTkButton(self, text="Add Task", command=self.add_task_event)
         self.button.grid(row=0, column=0, padx=10, pady=20, sticky="ew")
 
         # Create the checkbox_frame using the CheckboxFrame defined above
         self.checkbox_frame = CheckboxFrame(self, "Tasks", values=tasks)
         self.checkbox_frame.grid(row=1, column=0, padx=10, pady=(0,20), sticky="nesw")
         self.checkbox_frame.configure(fg_color="transparent")
+
+        # Create button to clear completed tasks
+        self.button = customtkinter.CTkButton(self, text="Clear Tasks", command=self.clear_tasks_event)
+        self.button.grid(row=2, column=0, padx=10, pady=(0,20), sticky="ew")
+
+        # Create a button to save current tasks
+        self.button = customtkinter.CTkButton(self, text="Save Tasks", command=self.save_tasks_event)
+        self.button.grid(row=3, column=0, padx=10, pady=(0,20), sticky="ew")
         
 
-    # Defines a function for the action of the button; needs finishing
-    def button_event(self):
+    # Defines a function for the Add Task button
+    def add_task_event(self):
         dialog = customtkinter.CTkInputDialog(text="Enter Task", title="Create Task")
         self.checkbox_frame.add_task(dialog.get_input())
-        #print(dialog.get_input())
-        #return task.task(dialog.get_input())
-        #print("checkbox_frame_1:", self.checkbox_frame.get_checked())
 
+    # Defines a funtion for the Clear Tasks button
+    def clear_tasks_event(self):
+        print("Clearing Tasks")
+
+    # Defines a function for the Save Tasks button
+    def save_tasks_event(self):
+        print("Saving Tasks")
 
 if __name__ == "__main__":
     # Create a dummy list of tasks; replace with a file read-in for task persistence
